ca0bd3d58139f14c441c18133dc2d14ac06e9f59
Brian Nyamweya <briangnyamweya@gmail.com>
2023-05-13 18:00:04 +0300

    major

--- src/products.js
+++ src/products.js
@@ -1,8 +1,8 @@
-import product1 from "./assets/products/1.png";
-import product2 from "./assets/products/2.png";
+import product1 from "./assets/products/1.jpeg";
+import product2 from "./assets/products/2.webp";
 import product3 from "./assets/products/3.png";
 import product4 from "./assets/products/4.png";
-import product5 from "./assets/products/5.png";
+import product5 from "./assets/products/3.jpg";
 import product6 from "./assets/products/6.webp";
 import product7 from "./assets/products/7.webp";
 import product8 from "./assets/products/8.webp";
@@ -10,7 +10,7 @@ import product8 from "./assets/products/8.webp";
 export const PRODUCTS = [
   {
     id: 1,
-    productName: "IPhone",
+    productName: "IPhone 14 pro max",
     price: 999.0,
     productImage: product1,
   },
